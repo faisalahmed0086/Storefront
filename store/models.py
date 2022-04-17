21966a11cff4318624fe009334933a04b40687c1
Faisal Ahmed <faisalahmed0086@gmail.com>
2022-04-17 08:16:56 +0600

    Resolving circular relationship relationship

--- store/models.py
+++ store/models.py
@@ -9,13 +9,14 @@ class Promotion(models.Model):
 
 class Collection(models.Model):
     title=models.CharField(max_length=255)
+    featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+')
 
 
 class Product(models.Model):
     title=models.CharField(max_length=255)
     descripttion= models.TextField()
     price= models.DecimalField(max_digits=2, decimal_places=2)
-    inventory=models.IntegerFieldField()
+    inventory=models.IntegerField()
     last_update=models.DateTimeField(auto_now=True)
     Collection=models.ForeignKey(Collection, on_delete=models.PROTECT)
     promotions=models.ManyToManyField(Promotion)
